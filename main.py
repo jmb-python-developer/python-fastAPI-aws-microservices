1447feddd47fb804fc5414b762df3d98674441cb
jmb-python-developer <165929314+jmb-python-developer@users.noreply.github.com>
2024-04-29 18:21:28 +0000

    Added fully functional endpoints

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 import uvicorn
 
 # Project's Logic Code
-from project_libs.logic import wiki
+from project_libs.logic import wiki, search_wiki
 
 app = FastAPI()
 """
@@ -14,15 +14,20 @@ Basic FastAPI application with two routes: the root route which responds with a 
 
 @app.get("/")
 async def root():
-    return {"message": "This is a Wikipedia API. Call paths /search or /wiki to use the functionality provided"}
+    return {"message": "This is a Wikipedia API. Call paths /search or /wiki to use the functionality provided. Use /docs for API developers documentation"}
 
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
-    total = num1 + num2
-    return {"total": total}
+@app.get("/search/{value}")
+async def search(value: str):
+    """Produces a search of keywords in wikipedia based on input value"""
+    search_results = search_wiki(value)
+    return {"Wikipedia Search Keywords" : search_results}
 
+@app.get("/wiki/{name}")
+async def search(name: str):
+    """Returns a wikipedia search with the name or keyword passed as criteria"""
+    wiki_result = wiki(name)
+    return {"Wiki Search Results": wiki_result}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
