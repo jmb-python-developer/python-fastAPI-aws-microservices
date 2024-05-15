840f7b060b8f916384e65e61192dbe8784c1337f
jmb-python-developer <jmb-python-developer@proton.me>
2024-05-15 11:24:57 +0200

    Adding one more test

--- test_main.py
+++ test_main.py
@@ -15,3 +15,12 @@ def test_read_main():
     assert response.json() == {
         "message": "This is a Wikipedia API. Call paths /search or /wiki to use the functionality provided. Use /docs for API developers documentation"
     }
+
+def test_phrase_main():
+    response = client.get("/wiki/Cadillac")
+    assert response.status_code == 200
+    assert response.json() == {
+    "Wiki Search Results": "Cadillac Motor Car Division, or simply Cadillac,  () is a division of the American automobile manufacturer General Motors (GM) that designs and builds luxury vehicles."
+    }
+
+# Other Unit Tests methods can be added here below ... 
